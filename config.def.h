8ac8f41b43899e4d1feff2ccdd9b9d11971e7b78
Markus Hanetzok <markus@hanetzok.net>
2023-05-07 11:55:17 +0200

    Revert "Applied colorbar patch"
    This reverts commit 4cb598e0cb1bf1a9b38fbd5add0bccd855da47d9.

--- config.def.h
+++ config.def.h
@@ -15,22 +15,15 @@ static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
-static const char col_black[]       = "#282a36";
-static const char col_gray[]        = "#44475a";
-static const char col_white[]       = "#f8f8f2";
-static const char col_yellow[]      = "#f1fa8c";
-static const char col_purple[]      = "#bd93f9";
-static const char col_pink[]        = "#ff79c6";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg              bg         border   */
-	[SchemeNorm]      = { col_white, col_black, col_black },
-	[SchemeSel]       = { col_white, col_black, col_purple  },
-
-	[SchemeStatus]    = { col_yellow, col_black,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel]   = { col_black,  col_yellow,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
-	[SchemeTagsNorm]  = { col_yellow, col_black,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-	[SchemeInfoSel]   = { col_pink, col_black,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-	[SchemeInfoNorm]  = { col_pink, col_black,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 typedef struct {
