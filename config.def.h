b9f189276a5532a15caec6caf3dc1c740f96e1f1
Markus Hanetzok <markus@hanetzok.net>
2023-05-07 12:37:53 +0200

    Changed color setting
    In order to make it easier to change color config in the future,
    variable names for colors are now more generic, eliminating the need to
    replace variables in the colors array and the dmenucmd options

--- config.def.h
+++ config.def.h
@@ -13,15 +13,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char norm_fg[]           = "#aaaaaa";
+static const char norm_bg[]           = "#282828";
+static const char norm_border[]       = "#282828";
+static const char sel_fg[]            = "#f2f2f2";
+static const char sel_bg[]            = "#282828";
+static const char sel_border[]        = "#0099aa";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { norm_fg, norm_bg, norm_border },
+	[SchemeSel]  = { sel_fg, sel_bg,  sel_border  },
 };
 
 typedef struct {
@@ -86,7 +87,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,  "-l", "10", "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,  "-l", "10", "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
